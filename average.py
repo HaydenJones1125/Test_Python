f684f22067724bce323e6ada7dfd82ff3c4e1d3e
Hayden Jones <>
2021-04-08 18:01:30 +0000

    Added Average

---
+++ average.py
@@ -0,0 +1,7 @@
+number_1 = input("Enter a number")
+number_2 = input("Enter another number")
+number_3 = input("Enter another number")
+number_4 = input("Enter another number")
+number_5 = input("Enter another number")
+numbers_added = number_1 + number_2 + number_3 + number_4 + number_5
+average = numbers_added / 5
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 answer = input("What is your favorite animal? ")
 print("Your favorite animal is a " + answer)
+
+number_1 = int(input("Enter a number: "))
+number_2 = int(input("Enter another number: "))
+number_3 = int(input("Enter another number: "))
+number_4 = int(input("Enter another number: "))
+number_5 = int(input("Enter another number: "))
+numbers_added = number_1 + number_2 + number_3 + number_4 + number_5
+average = numbers_added/5
+print("The average was " , average)
